c49507987cdbf474959bef12e535f699ec21cd5f
dipamsen <59444569+dipamsen@users.noreply.github.com>
2020-02-21 19:45:42 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,41 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            if(pointA.x>180){
+                strokeWeight(6);
+            } else {
+                strokeWeight(4);
+            }
+            stroke(48,22,8);
+            line(pointA.x-20, pointA.y+10, pointB.x, pointB.y);
+            line(pointA.x-20,pointA.y+10, 200,   pointB.y);
+            image(this.sling3,pointA.x-20,pointA.y,10,20);
+            
+        }
+        image(this.sling1,180,20);
+        image(this.sling2,150,20);
+
+    }
+    
+}
